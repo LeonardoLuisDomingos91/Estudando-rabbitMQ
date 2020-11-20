b7d59c4d8756e57e7c01abf51f6f376c3116a2c9
Leonardo Luis Domingos <you@example.com>
2020-11-20 10:30:52 -0300

    adicionando entidade employee

---
+++ src/main/java/br/com/rabbitgradle/RabbitGradleApplication.java
@@ -0,0 +1,13 @@
+package br.com.rabbitgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitGradleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/rabbitgradle/domain/Employee.java
@@ -0,0 +1,13 @@
+package br.com.rabbitgradle.domain;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private String name;
+
+    private String middleName;
+
+    private String lastName;
+}
