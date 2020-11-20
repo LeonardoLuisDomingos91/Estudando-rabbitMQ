b1401cdb1e84567b4a9b282917c5763c54c4112c
Leonardo Luis Domingos <you@example.com>
2020-11-20 14:00:31 -0300

    dev- populando a fila

--- src/main/java/br/com/rabbitgradle/RabbitGradleApplication.java
+++ src/main/java/br/com/rabbitgradle/RabbitGradleApplication.java
@@ -1,9 +1,12 @@
 package br.com.rabbitgradle;
 
+import br.com.rabbitgradle.channel.EmployeeChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding({EmployeeChannel.class})
 public class RabbitGradleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/rabbitgradle/controller/EmployeeContoller.java
+++ src/main/java/br/com/rabbitgradle/controller/EmployeeContoller.java
@@ -1,7 +1,30 @@
 package br.com.rabbitgradle.controller;
 
+import br.com.rabbitgradle.channel.EmployeeChannel;
+import br.com.rabbitgradle.domain.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+@Controller
+@RequestMapping("employee")
 public class EmployeeContoller {
+
+    @Autowired
+    private EmployeeChannel employeeChannel;
+
+    @PostMapping("/output")
+    public ResponseEntity<?> salvandoEmployee(@RequestBody Employee employee){
+        Message<Employee> messageEmployee = MessageBuilder
+                .withPayload(employee)
+                .build();
+        employeeChannel.output().send(messageEmployee);
+
+        return ResponseEntity.ok().build();
+    }
 }
